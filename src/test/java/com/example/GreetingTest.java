6fba75a40baddcaab1f62b4970a1e8aaa825075f
zack <sax4115@gmail.com>
2025-10-31 14:00:50 +0700

    [RED] Add failing test for testHelloBasic

---
+++ src/test/java/com/example/GreetingTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GreetingTest {
+
+    @Test
+    void testHelloBasic() {
+        assertEquals("Hello, World", "test failed");
+    }
+}
